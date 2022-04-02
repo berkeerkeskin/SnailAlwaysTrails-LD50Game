8ef1058f266b387bdfe6547011454bea8a3c15e1
Berke <efeberke1999@hotmail.com>
2022-04-02 17:06:56 +0300

    player run edited

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,56 +5,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller;
-    private Rigidbody2D playerRigidbody2D;
-    private CapsuleCollider2D capsuleCollider2D;
-    private float horizontalDirection;
-    public float speed = 1f;
+    private Rigidbody2D rb;
+    [SerializeField] private float
+        moveSpeed,
+        jumpSpeed;
+
+    private float
+        maxMoveSpeed = 10f,
+        baseMoveSpeed = 0.7f,
+        acceleration = 1f;
     void Start()
     {
         Debug.Log("Start");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = baseMoveSpeed;
     }
     
     private void Awake()
     {
-        playerRigidbody2D = GetComponent<Rigidbody2D>();
-        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalDirection = Input.GetAxisRaw("Horizontal");
-
-        Vector3 characterScale = transform.localScale;
-
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        { characterScale.x = -1;
-        }
-
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        { 
-            characterScale.x = 1;
-        }
-
-        transform.localScale = characterScale;
         
-        float moveDirection = 0;
-        if (Mathf.Abs(horizontalDirection) >= 0.0001f)
-            moveDirection =  0.9f;
     }
+
+    
 
     private void FixedUpdate()
     {
-        HandleMovement();
+        Run();
+        Jump();
         
     }
     
     
-    private void HandleMovement()
+    private void Run()
     {
-            playerRigidbody2D.velocity += new Vector2(horizontalDirection *speed, 0f);
-            //playerRigidbody2D.velocity = new Vector2(Mathf.Clamp(playerRigidbody2D.velocity.x, -speed, +speed), playerRigidbody2D.velocity.y);
-      
+        float directionX = Input.GetAxisRaw("Horizontal");
+
+        if (directionX == 0)
+        {
+            moveSpeed = baseMoveSpeed;
+        }
+        if (moveSpeed < maxMoveSpeed)
+        {
+            moveSpeed += acceleration;
+            rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
+        }else if (moveSpeed >= maxMoveSpeed)
+        {  
+            rb.velocity = new Vector2(directionX * maxMoveSpeed, rb.velocity.y);
+        }
+        
+        
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
     }
 }
