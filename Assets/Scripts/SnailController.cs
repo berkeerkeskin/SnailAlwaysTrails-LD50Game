0e32e8f4472b5d2cc4765cdfcf46bd96834b92e5
Berke <efeberke1999@hotmail.com>
2022-04-04 17:27:02 +0300

    slow down and speed up for snail is implemented.

--- Assets/Scripts/SnailController.cs
+++ Assets/Scripts/SnailController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SnailController : MonoBehaviour
@@ -22,9 +23,6 @@ public class SnailController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-        
-        
     }
 
     private void FixedUpdate()
@@ -40,6 +38,7 @@ public class SnailController : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
+        Debug.Log("speed: " + speed);
     }
 
 
@@ -47,5 +46,117 @@ public class SnailController : MonoBehaviour
     {
         targetPosition = GameObject.FindWithTag("Player").transform.position;
         return targetPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "cat food")
+        {
+            speed -= 0.3f;
+        }else if (other.tag == "cat litter")
+        {
+            speed -= 0.4f;
+        }else if (other.tag == "cereal")
+        {
+            speed -= 0.3f;
+        }else if (other.tag == "coffee mag")
+        {
+            speed -= 0.4f;
+        }else if (other.tag == "cologne")
+        {
+            speed -= 0.2f;
+        }else if (other.tag == "house")
+        {
+            speed -= 0.5f;
+        }else if (other.tag == "ice cream")
+        {
+            speed += 0.2f;
+        }else if (other.tag == "ice cubes")
+        {
+            speed += 0.5f;
+        }else if (other.tag == "koli")
+        {
+            speed -= 0.5f;
+        }else if (other.tag == "milk")
+        {
+            speed -= 0.2f;
+        }else if (other.tag == "mirror")
+        {
+            speed -= 0.4f;
+        }else if (other.tag == "oil")
+        {
+            speed += 0.3f;
+        }else if (other.tag == "salt")
+        {
+            speed -= 0.1f;
+        }else if (other.tag == "sawdust")
+        {
+            speed -= 0.5f;
+        }else if (other.tag == "soap")
+        {
+            speed += 0.3f;
+        }else if (other.tag == "stones")
+        {
+            speed -= 0.5f;
+        }else if (other.tag == "water")
+        {
+            speed -= 0.2f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "cat food")
+        {
+            speed += 0.3f;
+        }else if (other.tag == "cat litter")
+        {
+            speed += 0.4f;
+        }else if (other.tag == "cereal")
+        {
+            speed += 0.3f;
+        }else if (other.tag == "coffee mag")
+        {
+            speed += 0.4f;
+        }else if (other.tag == "cologne")
+        {
+            speed += 0.2f;
+        }else if (other.tag == "house")
+        {
+            speed += 0.5f;
+        }else if (other.tag == "ice cream")
+        {
+            speed -= 0.2f;
+        }else if (other.tag == "ice cubes")
+        {
+            speed -= 0.5f;
+        }else if (other.tag == "koli")
+        {
+            speed += 0.5f;
+        }else if (other.tag == "milk")
+        {
+            speed += 0.2f;
+        }else if (other.tag == "mirror")
+        {
+            speed += 0.4f;
+        }else if (other.tag == "oil")
+        {
+            speed -= 0.3f;
+        }else if (other.tag == "salt")
+        {
+            speed += 0.1f;
+        }else if (other.tag == "sawdust")
+        {
+            speed += 0.5f;
+        }else if (other.tag == "soap")
+        {
+            speed -= 0.3f;
+        }else if (other.tag == "stones")
+        {
+            speed += 0.5f;
+        }else if (other.tag == "water")
+        {
+            speed += 0.2f;
+        }
     }
 }
